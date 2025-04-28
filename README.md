2bdfbbaec1e68c272c0df80ad54c614aa7b47fec
Brandon Melendez <brandonmelendezpanana@gmail.com>
2025-04-27 19:33:25 -0500

    Mejoramiento de README y documentación de código

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Desafio técnico
+# Reto Técnico: Procesamiento de Transacciones Bancarias (CLI)
+
+## Introducción
+
+El siguiente programa tiene como proposito leer un archivo .csv con bastantes bastantes datos de transacciones bancarias de tipo Débito y Crédito y su respectivo monto, despues de analizar cada transacción se muestra en consola un reporte de los 3 puntos del desafio (Ver objetivo)
 
 ## Objetivo:
 
@@ -37,3 +41,20 @@ Desarrolla una aplicación de línea de comandos (CLI) que procese un archivo CS
 
 2. Una vez ejecutado se visualizara en consola automaticamente el resultado del desafio
    ![Resultado del Desafío](capturaResultado.png)
+
+## Enfoque y solución
+
+Se opto por usar la librería fs para poder leer el archivo data.csv y asu vez la librería csv-parse, para transformar el archivo a un formato en el que se pueda leer las columnas y poder leer cada fila para obtener como resultado un reporte final de las transacciones
+
+## Estructura del proyecto
+
+El proyecto se conforma por:
+- index.js (Ejecución del programa principal)
+- data.csv (Fuente de datos)
+- package.json (Dependencias e información del proyecto)
+- README (Instrucciones, objetivo y pasos de ejecución)
+- .gitignore (Ignora carpeta especificadas para no subir a github)
+- node_modules (Carpeta de npm, paqueterias)
+- capturaResultado.png (Imagen del resultado del reporte)
+
+Se opto por usar 1 solo archivo principal (index.js) donde se ejecuta toda la logica y el reporte.
--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const fs = require('fs');
 const csv = require('csv-parse');
 
+//Lectura del archivo csv
 fs.readFile('data.csv', 'utf-8', (err, data) => {
   if (err) {
     console.error('Error al leer el archivo csv, error: ', err);
     return;
   }
+  //Procesamiento del archivo csv
   csv.parse(
-    data,
+    data, //Datos a procesar
     {
       delimiter: ',', //Delimitador de columnas
       columns: true, //Si tiene encabezado
     },
-    (error, data) => {
+    (error, resultado) => {
       if (error) {
         console.error('Error procesando el archivo CSV:', error);
         return;
       }
-      reportes(data); //Llamar a la función reportes con los datos procesados
+      reportes(resultado); //Pasar los datos procesados a la función reportes
     }
   );
 });
 
-//Obtener:
-
-/*Balance Final:
-1) Suma de los montos de las transacciones de tipo "Crédito" menos la suma de los montos de las transacciones de tipo "Débito".
+/*
+Desafio Interbank Academy 25 - Procesamiento de transacciones:
+1)Balance Final: Suma de los montos de las transacciones de tipo "Crédito" menos la suma de los montos de las transacciones 
+de tipo "Débito".
 2) Transacción de Mayor Monto: Identificar el ID y el monto de la transacción con el valor más alto.
 3) Conteo de Transacciones: Número total de transacciones para cada tipo ("Crédito" y "Débito").
 */
 function reportes(data) {
+  // Inicialización de variables
   let montoCreditos = 0;
   let montoDebitos = 0;
 
@@ -39,11 +42,14 @@ function reportes(data) {
   let countCreditos = 0;
   let countDebitos = 0;
 
+  //Iteración de cada transacción
   data.forEach((objData) => {
+    //Validación para verificar el mayor monto e idenficar el id de transacción
     if (parseFloat(objData.monto) > mayorMonto) {
       mayorMonto = parseFloat(objData.monto);
       idTransaccion = objData.id;
     }
+    //Validación para verificar el tipo de transacción, contar los tipos de transacción y sumar los montos de cada tipo
     switch (objData.tipo) {
       case 'Crédito':
         montoCreditos += parseFloat(objData.monto);
@@ -57,8 +63,10 @@ function reportes(data) {
         console.log('Tipo de transacción no reconocido: ', objData.tipo);
     }
   });
+  //Calculo del balance final
   const balance = montoCreditos - montoDebitos;
 
+  //Impresión de resultados
   console.log('***************************************************************************************');
   console.log('El reporte de Procesamiento de transacciones es el siguiente:');
   console.log('balance final: ', balance.toFixed(2));
